5a1780519d59bccab3537b2e97e8b0cfecc21e8e
Cadillac <34432117+EmanuelBurgess@users.noreply.github.com>
2026-06-11 21:45:14 -0400

    Update S3 bucket name in backend configuration

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "actionspipeline1000"
+    bucket  = "YOURBUCKETNAME"
     key     = "actionspipeline1000/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
